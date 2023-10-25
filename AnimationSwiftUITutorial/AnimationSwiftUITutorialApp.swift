213a07a87073658722f6871d9b8228618ec4338f
ram4ik <ram4ik@hotmail.com>
2023-10-25 19:27:16 +0300

    Initial Commit

---
+++ AnimationSwiftUITutorial/AnimationSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftUITutorialApp.swift
+//  AnimationSwiftUITutorial
+//
+//  Created by admin on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
